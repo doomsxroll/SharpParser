465bbea4a2e778e8272a0711811e1dba64377354
unknown <doomsxcroll@users.noreply.github.com>
2025-12-12 15:46:03 +0000

    working on optional header parser

--- Parsers.cs
+++ Parsers.cs
@@ -44,67 +44,92 @@
             uint SizeOfUninitializedData = BitConverter.ToUInt32(inFile, (int)offset + 0xC);
             uint AddressOfEntryPoint = BitConverter.ToUInt32(inFile, (int)offset + 0x10);
             uint BaseOfCode = BitConverter.ToUInt32(inFile, (int)offset + 0x14);
-            
-            
-            uint BaseOfData = BitConverter.ToUInt32(inFile, (int)offset + 0x18);
-            
-            
-            uint ImageBase = BitConverter.ToUInt32(inFile, (int)offset + 0x1C);
-            uint SectionAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x20);
-            uint FileAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x24);
-            ushort MajorOperatingSystemVersion;
-            ushort MinorOperatingSystemVersion;
-            ushort MajorImageVersion;
-            ushort MinorImageVersion;
-            ushort MajorSubsystemVersion;
-            ushort MinorSubsystemVersion;
-            uint Win32VersionValue;
-            uint SizeOfImage;
-            uint SizeOfHeaders;
-            uint CheckSum;
-            ushort Subsystem;
-            ushort DllCharacteristics;
-            uint SizeOfStackReserve;
-            uint SizeOfStackCommit;
-            uint SizeOfHeapReserve;
-            uint SizeOfHeapCommit;
-            uint LoaderFlags;
-            uint NumberOfRvaAndSizes;
+
+            if (Magic == 0x10B)
+            { // PE32
+                uint BaseOfData = BitConverter.ToUInt32(inFile, (int)offset + 0x18);
+                uint ImageBase = BitConverter.ToUInt32(inFile, (int)offset + 0x1C);
+                uint SectionAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x20);
+                uint FileAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x24);
+                ushort MajorOperatingSystemVersion = BitConverter.ToUInt16(inFile, (int)offset + 0x28);
+                ushort MinorOperatingSystemVersion = BitConverter.ToUInt16(inFile, (int)offset + 0x2A);
+                ushort MajorImageVersion;
+                ushort MinorImageVersion;
+                ushort MajorSubsystemVersion;
+                ushort MinorSubsystemVersion;
+                uint Win32VersionValue;
+                uint SizeOfImage;
+                uint SizeOfHeaders;
+                uint CheckSum;
+                ushort Subsystem;
+                ushort DllCharacteristics;
+                uint SizeOfStackReserve;
+                uint SizeOfStackCommit;
+                uint SizeOfHeapReserve;
+                uint SizeOfHeapCommit;
+                uint LoaderFlags;
+                uint NumberOfRvaAndSizes;
+            }
+            else if (Magic == 0x20B)
+            { // PE32+
+                uint BaseOfData = 0;
+                uint ImageBase = BitConverter.ToUInt32(inFile, (int)offset + 0x1C);
+                uint SectionAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x20);
+                uint FileAlignment = BitConverter.ToUInt32(inFile, (int)offset + 0x24);
+                ushort MajorOperatingSystemVersion;
+                ushort MinorOperatingSystemVersion;
+                ushort MajorImageVersion;
+                ushort MinorImageVersion;
+                ushort MajorSubsystemVersion;
+                ushort MinorSubsystemVersion;
+                uint Win32VersionValue;
+                uint SizeOfImage;
+                uint SizeOfHeaders;
+                uint CheckSum;
+                ushort Subsystem;
+                ushort DllCharacteristics;
+                uint SizeOfStackReserve;
+                uint SizeOfStackCommit;
+                uint SizeOfHeapReserve;
+                uint SizeOfHeapCommit;
+                uint LoaderFlags;
+                uint NumberOfRvaAndSizes;
+            }
             // IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
 
-            return new OptionalHeader(
-                Magic,
-                MajorLinkerVersion,
-                MinorLinkerVersion,
-                SizeOfCode,
-                SizeOfInitializedData,
-                SizeOfUninitializedData,
-                AddressOfEntryPoint,
-                BaseOfCode,
-                BaseOfData,
-                ImageBase,
-                SectionAlignment,
-                FileAlignment,
-                MajorOperatingSystemVersion,
-                MinorOperatingSystemVersion,
-                MajorImageVersion,
-                MinorImageVersion,
-                MajorSubsystemVersion,
-                MinorSubsystemVersion,
-                Win32VersionValue,
-                SizeOfImage,
-                SizeOfHeaders,
-                CheckSum,
-                Subsystem,
-                DllCharacteristics,
-                SizeOfStackReserve,
-                SizeOfStackCommit,
-                SizeOfHeapReserve,
-                SizeOfHeapCommit,
-                LoaderFlags,
-                NumberOfRvaAndSizes
-            // IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-            );
+                return new OptionalHeader(
+                    Magic,
+                    MajorLinkerVersion,
+                    MinorLinkerVersion,
+                    SizeOfCode,
+                    SizeOfInitializedData,
+                    SizeOfUninitializedData,
+                    AddressOfEntryPoint,
+                    BaseOfCode,
+                    BaseOfData,
+                    ImageBase,
+                    SectionAlignment,
+                    FileAlignment,
+                    MajorOperatingSystemVersion,
+                    MinorOperatingSystemVersion,
+                    MajorImageVersion,
+                    MinorImageVersion,
+                    MajorSubsystemVersion,
+                    MinorSubsystemVersion,
+                    Win32VersionValue,
+                    SizeOfImage,
+                    SizeOfHeaders,
+                    CheckSum,
+                    Subsystem,
+                    DllCharacteristics,
+                    SizeOfStackReserve,
+                    SizeOfStackCommit,
+                    SizeOfHeapReserve,
+                    SizeOfHeapCommit,
+                    LoaderFlags,
+                    NumberOfRvaAndSizes
+                // IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+                );
         }
     }
 }
