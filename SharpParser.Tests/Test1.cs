88fbc9ffc8d19d5ad5648bb4746678a3fed16f5e
unknown <doomsxcroll@users.noreply.github.com>
2025-12-19 21:07:58 +0000

    tests

--- SharpParser.Tests/Test1.cs
+++ SharpParser.Tests/Test1.cs
@@ -1,11 +1,23 @@
-﻿namespace SharpParser.Tests
+﻿using SharpParser;
+
+namespace SharpParserTests
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class TestParsers
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DOSParser()
         {
+            byte[] TestDOSHeader = new byte[200];
+            TestDOSHeader[0] = 0x4D;
+            TestDOSHeader[1] = 0x5A;
+            TestDOSHeader[0x3c] = 0x64;
+            TestDOSHeader[0x66] = 0x45;
+            TestDOSHeader[0x67] = 0x50;
+            DOSHeader header = Parsers.ParseDOSHeader(TestDOSHeader);
+            Assert.AreEqual(Constants.DOSSignature, header.MagicNumber);
+            Assert.AreEqual(0x64u, header.SignatureLocation);
+            Assert.AreEqual(Constants.PESignature, header.Signature);
         }
     }
 }
